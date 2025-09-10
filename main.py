a13091dfedf40db4f38cc7225cc30dc21ccdd5d3
abhishekrajdhar <rajdhardubey4434@gmail.com>
2025-09-10 23:23:07 +0530

    adding cors

--- main.py
+++ main.py
@@ -3,12 +3,21 @@ from ultralytics import YOLO
 import uvicorn
 import shutil
 import os
-
+from fastapi.middleware.cors import CORSMiddleware
 # Load YOLOv11 classification model
 model = YOLO("best.pt")
 
 # Initialize FastAPI
 app = FastAPI(title="YOLOv11 Classification API", description="Serve YOLOv11 classification via FastAPI", version="1.0")
+
+# Allow requests from all origins (for testing)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],       # Allow all origins; change in production
+    allow_credentials=True,
+    allow_methods=["*"],       # Allow GET, POST, etc.
+    allow_headers=["*"],       # Allow headers like Content-Type, Authorization
+)
 
 @app.get("/")
 def home():
